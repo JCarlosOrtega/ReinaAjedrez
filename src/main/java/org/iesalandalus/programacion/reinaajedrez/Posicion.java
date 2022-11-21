61435390b8ae1c6b7a873d500e33157277f26c47
JCarlosOrtega <j.karlox@gmail.com>
2022-11-21 12:06:34 +0100

    Creados getters and setters de posición y validada la misma con lanzamiento de excepciones.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -7,4 +7,28 @@ public class Posicion {
 	
 	
 	
+	
+	
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8)
+			throw new IllegalArgumentException("ERROR: La fila introducida no es correcta");
+		else
+			this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna != 'a'|| columna != 'b' || columna != 'c' || columna != 'd' || columna != 'e' || columna != 'f' || columna != 'g' || columna != 'h')
+			throw new IllegalArgumentException("ERROR: La columna introducida no es correcta");
+		else
+			this.columna = columna;
+	}
+	
+	
+	
 }
