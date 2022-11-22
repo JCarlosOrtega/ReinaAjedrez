9267c54690a69e1fd04cd223854c27140ad3fd99
JCarlosOrtega <j.karlox@gmail.com>
2022-11-22 23:26:16 +0100

    Método elegirOpcionMenu y validación de elección

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,19 +1,33 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
+	
+	private int opcionElegida=0;
+	
 	
 	private Consola () {
 		
 	}
 	
-	public static void mostrarMenu () {
-		System.out.println("¡Bienvenido/a!");
-		System.out.println("¿Qué acción quieres realiza?");
-		System.out.println("Elige entre las opciones 1 - 4");
+	public void mostrarMenu () {
+		
+
 		System.out.println("1. Crear una Reina por defecto.");
 		System.out.println("2. Crear una Reina eligiendo color.");
 		System.out.println("3. Mover.");
 		System.out.println("4. Salir.");
 	}
 
+	public int elegirOpcionMenu () {
+		mostrarMenu();
+		System.out.println("¿Qué acción quieres realizar?");
+		do {
+		System.out.println("Elige entre las opciones 1 - 4");
+		opcionElegida=Entrada.entero();
+		} while (opcionElegida < 1 || opcionElegida > 4);
+		
+		return opcionElegida;
+	}
 }
